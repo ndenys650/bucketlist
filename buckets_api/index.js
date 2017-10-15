a3bf9476b67c168b0db92e3b41f27b8352af886b
Nate Denys <nate_denys@yahoo.com>
2017-10-15 14:40:14 -0700

    create front end of app, prepare for ajax calls
    created index.html, css styling, and add script tag for jquery

--- buckets_api/index.js
+++ buckets_api/index.js
@@ -7,9 +7,11 @@ var bucketRoutes = require('./routes/buckets')
     
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/views'))
 
 app.get('/', function(req, res){
-    res.send("Hi from root route!");
+    res.sendFile("index.html");
 });
 
 app.use('/api/buckets', bucketRoutes);
