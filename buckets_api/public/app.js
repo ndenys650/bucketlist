cc160e68e3555bb3e0e064630b108c3c9a53fa47
Nate Denys <nate_denys@yahoo.com>
2017-10-15 18:41:17 -0700

    customize front end, confirm connection to back end,

--- buckets_api/public/app.js
+++ buckets_api/public/app.js
@@ -1,1 +1,14 @@
-alert("hi from app.js")
+$(document).ready(function(){
+    $.getJSON('/api/buckets')
+    .then(addBuckets)
+});
+
+function addBuckets(buckets){
+    buckets.forEach(function(bucket){
+        var newBucket = $('<li class="task">' + bucket.name + '</li>');
+        if(bucket.completed){
+            newBucket.addClass("done");
+        }
+        $('.list').append(newBucket);
+    });
+}
